18daccff72e85b26bfc787a16df683daf46aa4f1
vins-stha <shrs_samar@yahoo.com>
2022-04-13 15:34:27 +0300

    updated components

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,17 +1,36 @@
 import React from 'react';
+import Carousel from "react-elastic-carousel";
 
-export const Carousel = () => {
+export const ImageCarousel = (params) => {
+    const {baseurl, curCatImgs, curCatName, data, rotate, rotation} = params;
     return (
         <>
             <div className={"flex-column carousel-body"}>
                 <div className={"image-body "}>
-                    <img  className={""} src="image-body" alt=""/>
+                    {curCatImgs?.length > 0 ? (
+
+                        <Carousel className={"bigg"}>
+                            {
+                                curCatImgs?.map((image, id) =>
+                                    <img
+                                        className="scrollingImage"
+                                        src={baseurl + data[0]?.root + curCatName + "/" + image}
+                                        style={{transform: `rotate(${rotation}deg)`}}
+                                        alt={image}
+                                        key={id}
+                                    />)
+                            }
+                        </Carousel>
+                    ) : (<h2>No images available</h2>)}
+
                 </div>
-                <div className={"carousel-actions flex"}>
-                    <button>Prev</button>
-                    <button>Rotate</button>
-                    <button>Next</button>
-                </div>
+                {curCatImgs?.length > 0 && (
+                    <div className={"carousel-actions flex"}>
+
+                        <button onClick={rotate}>Rotate</button>
+
+                    </div>
+                )}
 
             </div>
         </>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 
-export const Navbar = () => {
-	return (
-		<>
-			<div className={"flex"}>
-				<div>
-					<button>Prev</button>
-				</div>
-				<div>
-					<h3 className={"catName"}> Category title</h3>
-				</div>
-				<div>
-					<button>Next</button>
-				</div>
-			</div>
-		</>
-	)
+export const Navbar = (params) => {
+    const {getPrevCat, getNextCat, curCatName} = params;
+    return (
+        <>
+            <div className={"flex navbar"}>
+                <div>
+                    <button onClick={getPrevCat}>Prev</button>
+                </div>
+                <div>
+                    <h3 className={"catName"}> Category title {curCatName}</h3>
+                </div>
+                <div>
+                    <button onClick={getNextCat}>Next</button>
+                </div>
+            </div>
+        </>
+    )
 }
